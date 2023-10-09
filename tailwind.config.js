04cd00f060c9d562ea42ca87f23413eb4c94e1aa
Juan Verrel Tanuwijaya <juan.121140072@student.itera.ac.id>
2023-10-09 23:53:36 +0700

    making hero section & about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,20 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
       colors: {
         primary: '#14b8a6',
-        dark: '#020617'
+        secondary: '#64748b',
+        dark: '#020617',
+      },
+      screens:{
+        '2xl': '1320px',
       }
-    },
+    }
   },
   plugins: [],
 };
