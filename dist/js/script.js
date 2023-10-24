1b01dce868c97d2b45dfe01e33e084d38118f8b2
Juan Verrel Tanuwijaya <126883141+Juan-121140072@users.noreply.github.com>
2023-10-24 14:42:53 +0700

    adding navbar features

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,21 @@
+// Navbar Fixed
+window.onscroll= ()=>{
+    const header=document.querySelector("header");
+    const fixedNav= header.scrollTop;
+
+    if(window.scrollY > fixedNav){
+        header.classList.add("navbar-fixed");
+    }else{
+        header.classList.remove("navbar-fixed");
+    }
+}
+
 // Hamburger
 
 const hamburger=document.querySelector("#hamburger");
+const navMenu=document.querySelector("#nav-menu");
 
 hamburger.addEventListener("click",()=>{
     hamburger.classList.toggle("hamburger-active");
+    navMenu.classList.toggle("hidden");
 });
